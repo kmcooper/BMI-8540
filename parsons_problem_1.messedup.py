25d811b6fadc7eb86e48d926d61defce4883e791
kmcooper <kmcooper@unomaha.edu>
2021-02-03 11:16:47 -0600

    Create parsons_problem_1.messedup.py

---
+++ parsons_problem_1.messedup.py
@@ -0,0 +1,14 @@
+print(destinations)
+packages_count = 9
+print("They weight a total of " + str(total_weight) + " pounds")
+total_weight = 180
+                'Wisconsin', 'Iowa', 'Michigan', 'California']
+print("Welcome to  the Post Office!")
+destinations = []
+destinations = ['Florida', 'New Mexico', 'Georgia', 'Alaska', 'Texas', 
+
+        " packages today\n")
+destinations.sort()
+print("Heres a list of destinations for each, in alphabetical order:\n")
+print("It looks like you are mailing " + str(packages_count) + \
+#!/usr/bin/python3.7
